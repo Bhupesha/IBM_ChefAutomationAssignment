ea44a613c4a0135105b0b93c4bc6d1951a4916f3
bhupesh <boopi.ht@gmail.com>
2017-12-04 23:29:43 +0530

    Added package httpd  configuraion in recipe

--- test/smoke/default/server_test.rb
+++ test/smoke/default/server_test.rb
@@ -4,3 +4,16 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+
+package_name =
+  service_name =
+    case node['platform']
+    when 'centos' then 'httpd'
+    #when 'ubuntu' then 'apache2'
+    end
+
+package package_name
+
+service service_name do
+  action [:enable, :start]
+end
