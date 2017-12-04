03cfdc4dfdccad4af14480c2ebdfc34e573fa213
bhupesh <boopi.ht@gmail.com>
2017-12-04 23:37:33 +0530

    Changed package configuration from server_test.rb to server.rb under recipe folder and tested by adding Simple test case

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Checking package httpd has been installed
+
+package_name =
+  service_name =
+    case node['platform']
+    when 'centos' then 'httpd'
+    #when 'ubuntu' then 'apache2'
+    end
+
+package package_name
+
+service service_name do
+  action [:enable, :start]
+end
--- test/smoke/default/server_test.rb
+++ test/smoke/default/server_test.rb
@@ -5,15 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-package_name =
-  service_name =
-    case node['platform']
-    when 'centos' then 'httpd'
-    #when 'ubuntu' then 'apache2'
-    end
-
-package package_name
-
-service service_name do
-  action [:enable, :start]
+describe package('httpd') do
+  it { should be_installed }
 end
