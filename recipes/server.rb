79417cd61a5bf6c30565fa7f2c1f251d8aa141e5
Bhupesha <31911728+Bhupesha@users.noreply.github.com>
2018-03-11 14:06:02 +0530

    Update server.rb
    Updated recipe

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,16 +1,8 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
-# Checking package httpd has been installed
-
 package_name =
   service_name =
     case node['platform']
     when 'centos' then 'httpd'
-    #when 'ubuntu' then 'apache2'
+    when 'ubuntu' then 'apache2'
     end
 
 package package_name
@@ -20,10 +12,23 @@ service service_name do
 end
 
 #custom home page.
+# when we want to access Attribute in recipe declare default['greeting'] = 'bhupesh' in /attribute/webatt.rb
+# and within Double Quotes block of Statement <html>#{node['greeting']}</html>
 file '/var/www/html/index.html' do
-  content '<html>
+  content "<html>
   <body>
     <h1>Welcome IBM</h1>
+    <h1>#{node['greeting']}</h1>
+    <h1>#{node['hostname']}</h1>
+    <h1>#{node['ipaddress']}</h1>
+    <h1>#{node['fqdn']}</html>
   </body>
-</html>'
+</html>"
+end
+
+file "/var/lib/#{node['fileName']}" do
+end
+
+template 'var/www/html/index.html' do
+  source 'index.html.erb'
 end
